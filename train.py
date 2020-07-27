92ef503390539963c60138a4bb07831bdbf252d4
ruofanliu <kelseyliu1988@gmail.com>
2020-07-27 17:21:06 +0800

    Changed train.py

--- train.py
+++ train.py
@@ -149,11 +149,11 @@ def train(img_folder, logo_size, orientations, pixels_per_cell, cells_per_block)
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser()
-    parser.add_argument('-data', "--data_folder", help='Training brands\' screenshots to create positive samples', default= 'D:/ruofan/git_space/phishpedia/benchmark/Train_5brand')
-    parser.add_argument('-annot', "--annot_file", help='Annotation for training brands', default='D:/ruofan/git_space/phishpedia/benchmark/phish1000_coord.txt' )
-    parser.add_argument('-outf', '--output_folder', help='Created training folder', default='D:/ruofan/git_space/phishpedia/benchmark/SVM_imageset')
-    parser.add_argument('-benignf', '--benign_folder', help='Benign screenshots to create negative samples', default='D:/ruofan/git_space/phishpedia/benchmark/test15k_wo_localcontent/benign_sample_15k')
-    parser.add_argument('-ratio', '--posneg_ratio', type=int, default=3)
+    parser.add_argument('-d', "--data_folder", help='Training brands\' screenshots to create positive samples', default= 'D:/ruofan/git_space/phishpedia/benchmark/Train_5brand')
+    parser.add_argument('-a', "--annot_file", help='Annotation for training brands', default='D:/ruofan/git_space/phishpedia/benchmark/phish1000_coord.txt' )
+    parser.add_argument('-o', '--output_folder', help='Created training folder', default='D:/ruofan/git_space/phishpedia/benchmark/SVM_imageset')
+    parser.add_argument('-b', '--benign_folder', help='Benign screenshots to create negative samples', default='D:/ruofan/git_space/phishpedia/benchmark/test15k_wo_localcontent/benign_sample_15k')
+    parser.add_argument('-r', '--posneg_ratio', type=int, default=3)
     args = parser.parse_args()
     
     data_creation(args.data_folder, args.annot_file, args.output_folder, args.benign_folder, args.posneg_ratio)
